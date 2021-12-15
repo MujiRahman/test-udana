66ed07ce232a5281e94dc2d4e9673035052c7aca
MujiRahman <mujirahman888@gmail.com>
2021-12-16 00:54:28 +0700

    membuat banner buat web tes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { bgIimage, logo } from "./assets";
 
 function App() {
+  const [tampil, setTampil] = useState(false);
+  const tampilkan = () => {
+    if (tampil === true) {
+      setTampil(false);
+    } else {
+      setTampil(true);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div>
+        <img
+          src={bgIimage}
+          alt="bgImage"
+          className=" h-screen w-screen z-10 rounded-bl-3xl md:rounded-bl-[150px]"
+        />
+        <div className="w-full bg-yellow-500 md:h-44 -mt-40 rounded-bl-full"></div>
+        <div class=" absolute top-2 right-0 left-0 flex justify-around p-2.5 lg:flex ">
+          <div className=" flex gap-6 text-sm text-white font-semibold lg:-ml-32 ">
+            <img src={logo} alt="logo udana" class=" w-36 h-9" />
+            <p className="hidden md:block p-2">WaraLaba</p>
+            <p className="hidden md:block p-2">FAQ</p>
+            <p className="hidden md:block p-2">Tentang</p>
+          </div>
+          <div className="hidden md:flex text-sm text-white font-semibold gap-6 lg:-mr-40">
+            <p className="p-2 text-center">Masuk</p>
+            <p className="p-2 text-center bg-yellow-400 rounded-xl">
+              Mulai Daftar
+            </p>
+          </div>
+          <div class="space-y-1 mt-2 md:hidden" onClick={tampilkan}>
+            <span class="block w-6 h-0.5 bg-white"></span>
+            <span class="block w-6 h-0.5 bg-white"></span>
+            <span class="block w-6 h-0.5 bg-white"></span>
+          </div>
+        </div>
+        <div
+          className={
+            tampil
+              ? "w-11/12  absolute top-16 left-3.5 right-3 flex flex-col gap-2 md:hidden "
+              : "hidden md:hidden"
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="h-28 text-sm text-white font-semibold bg-gray-500 ">
+            <hr className=" bg-yellow-500 -mt-2" />
+            <p className="p-4">Masuk</p>
+            <p className="p-3 text-center rounded-xl bg-yellow-400 ">
+              Mulai Daftar
+            </p>
+          </div>
+          <div className="bg-gray-500 text-sm text-white font-semibold">
+            <p className="p-4">WaraLaba</p>
+            <p className="p-4">FAQ</p>
+            <p className="p-4">Tentang</p>
+          </div>
+        </div>
+        <div className="z-20 p-6 text-white -mt-96 lg:w-1/2 ml-28">
+          <h1 className=" text-3xl font-bold -mt-4 mb-6 md:-mt-24">
+            Jadilah Investor UMKM & Waralaba
+          </h1>
+          <p className="leading-7 md:text-2xl my-4">
+            Temukan bisnis UMKM & Waralaba yang anda sukai dan mulai menjadi
+            investor untuk kemajuan perekonomian bangsa tanpa perlu pusing
+            dengan operasional sehari-hari
+          </p>
+          <button className="p-3 my-6 bg-yellow-500 rounded-xl md:text-2xl mt-4 ">
+            Investasi Sekarang -->
+          </button>
+        </div>
+      </div>
+      <div className="flex"></div>
+    </>
   );
 }
 
---
+++ src/assets/index.js
@@ -0,0 +1,39 @@
+import bgIimage from "./images/bg-image.jpg";
+import bisnis1 from "./images/bisnis1.png";
+import bisnis2 from "./images/bisnis2.png";
+import bisnis3 from "./images/bisnis3.png";
+import bisnis4 from "./images/bisnis4.png";
+import bisnis5 from "./images/bisnis5.png";
+import bisnis6 from "./images/bisnis6.png";
+import card1 from "./images/card1.png";
+import card2 from "./images/card2.png";
+import card3 from "./images/card3.png";
+import sm1 from "./images/sm1.png";
+import sm2 from "./images/sm2.png";
+import support1 from "./images/support1.png";
+import support2 from "./images/support2.png";
+import support3 from "./images/support3.png";
+import support4 from "./images/support4.png";
+import logo from "./images/udana.png";
+import udanaFooter from "./images/udanaFooter.png";
+
+export {
+  logo,
+  bisnis1,
+  bisnis2,
+  bisnis3,
+  bisnis4,
+  bisnis5,
+  bisnis6,
+  card1,
+  card2,
+  card3,
+  sm1,
+  sm2,
+  support1,
+  support2,
+  support3,
+  support4,
+  udanaFooter,
+  bgIimage,
+};
